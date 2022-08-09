36844cd46ee7b587d27c4a87f16a5a9ed74b21c1
lemon0922 <ima.com0922@icloud.com>
2022-08-09 15:39:49 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,    presence: true
+  validates :first_name,  presence: true
+  validates :family_name, presence: true
+  validates :read_first,  presence: true
+  validates :read_family, presence: true
+  validates :birth,       presence: true
+
+  has_many :items
+  has_many :orders
 end
